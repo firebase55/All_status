558c821443f6b7850ff0a3cc2672ce2c3e6e6b48
DESKTOP-1H6TP8L <firebase732@gmail.com>
2020-10-20 08:30:06 -0700

    Initial commit

---
+++ app/src/main/java/com/allin/onedownloader/interfaces/UserListInterface.java
@@ -0,0 +1,8 @@
+package com.allin.onedownloader.interfaces;
+
+
+import com.allin.onedownloader.model.story.TrayModel;
+
+public interface UserListInterface {
+    void userListClick(int position, TrayModel trayModel);
+}
---
+++ app/src/main/java/com/allin/onedownloader/util/ClipboardListener.java
@@ -0,0 +1,7 @@
+package com.allin.onedownloader.util;
+
+import android.content.ClipboardManager;
+
+public abstract class ClipboardListener implements ClipboardManager.OnPrimaryClipChangedListener {
+
+}
